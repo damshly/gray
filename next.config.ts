7d7d4ebd6d200fc07a191cb826677831c3972390
damshly <damshly@proton.me>
2026-08-30 16:45:44 +0300

    ci: configure static export and hostinger ftp deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // مطلوب عند استخدام التصدير الإستاتيكي مع Next/Image
+  },
 };
 
 export default nextConfig;
